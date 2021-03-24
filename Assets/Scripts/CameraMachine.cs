9ee19145abddd8133a32031fa875e0e4123c49bf
Joe Nicholson <joeqnicholson@gmail.com>
2021-03-23 23:01:51 -0400

    Added environment prop sprites

--- Assets/Scripts/CameraMachine.cs
+++ Assets/Scripts/CameraMachine.cs
@@ -23,6 +23,9 @@ public class CameraMachine : MonoBehaviour
 
         if (machine.currentCameraBox)
         {
+            
+
+
             Target = new Vector3(
                     currentCameraBox.onTrackX ? machine.transform.position.x : boxTransform.position.x,
                     currentCameraBox.onTrackY ? machine.transform.position.y : boxTransform.position.y,
@@ -31,21 +34,23 @@ public class CameraMachine : MonoBehaviour
 
             Target.x = Mathf.Clamp(
             Target.x,
-            boxTransform.position.x - (boxTransform.localScale.x / 2f) + 112.5f,
-            boxTransform.position.x + (boxTransform.localScale.x / 2f) - 112.5f
+            boxTransform.position.x - (boxTransform.localScale.x / 2f) +240,
+            boxTransform.position.x + (boxTransform.localScale.x / 2f) -240 
         );
 
             Target.y = Mathf.Clamp(
                 Target.y,
-                boxTransform.position.y - (boxTransform.localScale.y / 2f) + 112.5f,
-                boxTransform.position.y + (boxTransform.localScale.y / 2f) - 112.5f
+                boxTransform.position.y - (boxTransform.localScale.y / 2f) + 135f,
+                boxTransform.position.y + (boxTransform.localScale.y / 2f) - 135f
             );
-
-            transform.position = Vector3.Lerp(transform.position, Target, 6 * Time.deltaTime);
-            
-                    
+      
         }
+        transform.position = Vector3.Lerp(transform.position, Target, 4 * Time.deltaTime);
 
+    }
+
+    void LateUpdate()
+    {
         
     }
 
